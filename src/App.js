ba8aaeba5f5f96d75394ce01a2a4ff1015987392
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2022-09-30 14:28:43 +0545

    registration styling complete

--- src/App.js
+++ src/App.js
@@ -38,9 +38,36 @@ const randomTeam = availibleTeams[Math.floor(Math.random() * availibleTeams.leng
 console.log(randomTeam)
 function App() {
   return (
-    <div className="App">
-  
+<>
+  <div className="container">
+    <div className="title">2022 World Cup Sweepstakes</div>
+    <div className="content">
+      <form action="#">
+        <div className="user-details">
+          <div className="input-box">
+            <span className="details">Full Name</span>
+            <input type="text" placeholder="Enter your name" required />
+          </div>
+          <div className="input-box">
+            <span className="details">Email</span>
+            <input type="text" placeholder="Enter your email" required />
+          </div>
+          <div className="input-box">
+            <span className="details">Password</span>
+            <input type="text" placeholder="Enter your password" required />
+          </div>
+          <div className="input-box">
+            <span className="details">Confirm Password</span>
+            <input type="text" placeholder="Confirm your password" required />
+          </div>
+        </div>
+        <div className="button">
+          <input type="submit" value="Register" />
+        </div>
+      </form>
     </div>
+  </div>
+</>
   );
 }
 
