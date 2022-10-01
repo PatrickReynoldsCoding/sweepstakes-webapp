8cff56a0613f476b241c981fb053d0873f2700d7
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2022-10-01 15:00:29 +0545

    routing added and registration made into componant

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import { useState, useEffect, useRef } from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom';
 import { useForm } from "react-hook-form";
 import "./App.css";
+
+
+// Components
+import Registration from "./components/Registration";
+
 // import { db } from "./firebase-config";
 // import {
 //   collection,
@@ -14,6 +20,7 @@ import "./App.css";
 
 //functions
 const teamPicker = require("./functions/teamPicker");
+
 
 //dummyData
 const teams = [
@@ -38,93 +45,14 @@ const teams = [
 console.log(teamPicker(teams));
 
 function App() {
-  const {
-    register,
-    handleSubmit,
-    watch,
-    formState: { isValid },
-  } = useForm({
-    mode: "onChange",
-  });
-
-  const onSubmit = (data) => {
-    console.log(data);
-  }; // your form submit function which will invoke after successful validation
 
   return (
-    <>
-      <div className="container">
-        <div className="title">2022 World Cup Sweepstakes</div>
-        <div className="content">
-          {/* change action to redirect */}
-          <form
-            onSubmit={(e) => {
-              handleSubmit(onSubmit)(e)
-                // you will have to catch those error and handle them
-                .catch(() => {});
-            }}
-          >
-            <div className="user-details">
-              {/* name */}
-              <div className="input-box">
-                <span className="details">Full Name</span>
-                <input
-                  type="text"
-                  placeholder="Enter your name"
-                  {...register("name", { required: true })}
-                />
-              </div>
-              {/* email */}
-              <div className="input-box">
-                <span className="details">Email</span>
-                <input
-                  type="email"
-                  placeholder="Enter your email"
-                  {...register("email", { required: true })}
-                />
-              </div>
-              {/* password */}
-              <div className="input-box">
-                <span className="details">Password</span>
-                <input
-                  type="password"
-                  placeholder="Enter your password"
-                  {...register("password", { required: true })}
-                />
-                {/* {errors?.password?.type === "required" && (
-                  <p>This field is required</p>
-                )} */}
-              </div>
-              {/* confirm password */}
-              <div className="input-box">
-                <span className="details">Confirm Password</span>
-                <input
-                  type="password"
-                  placeholder="Confirm your password"
-                  {...register("password_repeat", {
-                    required: true,
-                    validate: (val) => {
-                      if (watch("password") != val) {
-                        return false;
-                      }
-                    },
-                  })}
-                />
-                {/* {errors?.password_repeat?.type === "required" && (
-                  <p>This field is required</p>
-                )} */}
-              </div>
-            </div>
-            {watch("password_repeat") !== watch("password") && (
-              <p>password do not match</p>
-            )}
-            <div className="button">
-              <input type="submit" value="Register" hidden={!isValid} />
-            </div>
-          </form>
-        </div>
-      </div>
-    </>
+    <BrowserRouter>
+
+      <Routes>
+        <Route path="/registration" element={<Registration />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/components/Registration.js
@@ -0,0 +1,96 @@
+import { useForm } from "react-hook-form";
+import "../App.css";
+
+
+function Registration() {
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { isValid },
+  } = useForm({
+    mode: "onChange",
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  }; // your form submit function which will invoke after successful validation
+
+  return (
+    <>
+      <div className="container">
+        <div className="title">2022 World Cup Sweepstakes</div>
+        <div className="content">
+          {/* change action to redirect */}
+          <form
+            onSubmit={(e) => {
+              handleSubmit(onSubmit)(e)
+                // you will have to catch those error and handle them
+                .catch(() => {});
+            }}
+          >
+            <div className="user-details">
+              {/* name */}
+              <div className="input-box">
+                <span className="details">Full Name</span>
+                <input
+                  type="text"
+                  placeholder="Enter your name"
+                  {...register("name", { required: true })}
+                />
+              </div>
+              {/* email */}
+              <div className="input-box">
+                <span className="details">Email</span>
+                <input
+                  type="email"
+                  placeholder="Enter your email"
+                  {...register("email", { required: true })}
+                />
+              </div>
+              {/* password */}
+              <div className="input-box">
+                <span className="details">Password</span>
+                <input
+                  type="password"
+                  placeholder="Enter your password"
+                  {...register("password", { required: true })}
+                />
+                {/* {errors?.password?.type === "required" && (
+                  <p>This field is required</p>
+                )} */}
+              </div>
+              {/* confirm password */}
+              <div className="input-box">
+                <span className="details">Confirm Password</span>
+                <input
+                  type="password"
+                  placeholder="Confirm your password"
+                  {...register("password_repeat", {
+                    required: true,
+                    validate: (val) => {
+                      if (watch("password") != val) {
+                        return false;
+                      }
+                    },
+                  })}
+                />
+                {/* {errors?.password_repeat?.type === "required" && (
+                  <p>This field is required</p>
+                )} */}
+              </div>
+            </div>
+            {watch("password_repeat") !== watch("password") && (
+              <p>password do not match</p>
+            )}
+            <div className="button">
+              <input type="submit" value="Register" hidden={!isValid} />
+            </div>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Registration;
