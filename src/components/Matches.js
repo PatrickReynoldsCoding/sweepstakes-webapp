746f4892c14bd650f1cc4bf6b89271691139f16f
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2022-10-13 11:14:11 +0545

    Login API refactored

--- src/components/Matches.js
+++ src/components/Matches.js
@@ -1,28 +1,12 @@
 import React, { useEffect } from "react";
 import "./Matches.css";
 
+
+//functions
+import { login } from "../functions/worldCupApi/login";
+
 export default function Matches() {
-  const login = async () => {
-    try {
-      const res = await fetch("/login", {
-        method: "POST",
-        body: JSON.stringify({
-          email: "patrickreynoldscoding@gmail.com",
-          password: "password",
-        }),
-        headers: {
-          "Content-Type": "application/json",
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Headers": "Origin, X-Requested-With, Content-Type, Accept"
-        },
-      
-      })      
-      .then((res) => res.json())
-      .then((data) => {
-        console.log(data);
-      });;
-    } catch (err) {}
-  };
+
 
   useEffect(() => {
     login()
