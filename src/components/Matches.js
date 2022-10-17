f5516e05a3b0da4f2ea0ef87d988608c903e562b
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2022-10-17 15:12:54 +0545

    data working

--- src/components/Matches.js
+++ src/components/Matches.js
@@ -1,26 +1,22 @@
 import React, { useEffect, useState } from "react";
 import "./Matches.css";
 
-      const getMatchData = async (token) => {
-        // console.log(`hello ${token}`)
-    
-        try {
-          const res = await fetch("/match", {
-            method: "GET",
-            headers: {
-             Authorization: `Bearer ${token}`,
-              "Content-Type": "application/json",
-              "Access-Control-Allow-Origin": "*",
-              "Access-Control-Allow-Headers":
-                "Origin, X-Requested-With, Content-Type, Accept",
-            },
-          })
-            .then((res) => res.json())
-            .then((data) => {
-              console.log(data);
-            });
-        } catch (err) {}
-      };
+const getMatchData = async (token) => {
+  // console.log(`hello ${token}`)
+
+  try {
+    return fetch("/match", {
+      method: "GET",
+      headers: {
+        Authorization: `Bearer ${token}`,
+        "Content-Type": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        "Access-Control-Allow-Headers":
+          "Origin, X-Requested-With, Content-Type, Accept",
+      },
+    }).then((res) => res.json());
+  } catch (err) {}
+};
 
 const getToken = async () => {
   return fetch("/user/login", {
@@ -40,17 +36,15 @@ const getToken = async () => {
 
 export default function Matches() {
   const [matchData, setMatchData] = useState(null);
-
-const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzQ3OGJiYWRhYTlhZmYzZTc1YTRmNzkiLCJpYXQiOjE2NjU5ODkzNTcsImV4cCI6MTY2NjA3NTc1N30.4xaff7lqxqT38rvO-01xcwPhxxgtw7-_Yq8YzqJVfXU"        
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-
- getToken()
-  .then((res) => getMatchData(res.data.token))
-  .then(setMatchData)
-  .catch(console.error)
-// getMatchData(token)
+    getToken()
+      .then((res) => getMatchData(res.data.token))
+      .then((res) => setMatchData(res.data))
+      .then(() => setLoading(false))
+      .catch(console.error);
   }, []);
 
-  return <div>{matchData}</div>;
+  return <div>{loading ? "loading" : JSON.stringify(matchData)}</div>;
 }
