3e51fb1be74f9d671b2b716109f0f156c8bb6563
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2022-10-17 12:35:18 +0545

    token as argument works

--- src/components/Matches.js
+++ src/components/Matches.js
@@ -1,20 +1,26 @@
 import React, { useEffect, useState } from "react";
 import "./Matches.css";
 
-const getMatchData = async (token) => {
-  console.log(token.data.token)
-  return fetch("/match", {
-    method: "GET",
-    headers: {
-      Authentication: `Bearer ${token.data.token}`,
-      "Content-Type": "application/json",
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Headers":
-        "Origin, X-Requested-With, Content-Type, Accept",
-    },
-  })
-    .then((res) => res.json())
-};
+      const getMatchData = async (token) => {
+        // console.log(`hello ${token}`)
+    
+        try {
+          const res = await fetch("/match", {
+            method: "GET",
+            headers: {
+             Authorization: `Bearer ${token}`,
+              "Content-Type": "application/json",
+              "Access-Control-Allow-Origin": "*",
+              "Access-Control-Allow-Headers":
+                "Origin, X-Requested-With, Content-Type, Accept",
+            },
+          })
+            .then((res) => res.json())
+            .then((data) => {
+              console.log(data);
+            });
+        } catch (err) {}
+      };
 
 const getToken = async () => {
   return fetch("/user/login", {
@@ -35,11 +41,10 @@ const getToken = async () => {
 export default function Matches() {
   const [matchData, setMatchData] = useState(null);
 
+const token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MzQ3OGJiYWRhYTlhZmYzZTc1YTRmNzkiLCJpYXQiOjE2NjU5ODkzNTcsImV4cCI6MTY2NjA3NTc1N30.4xaff7lqxqT38rvO-01xcwPhxxgtw7-_Yq8YzqJVfXU"        
+
   useEffect(() => {
-    getToken()
-      .then(getMatchData)
-      // .then(setMatchData)
-      .catch(console.error)
+getMatchData(token)
   }, []);
 
   return <div>{matchData}</div>;
