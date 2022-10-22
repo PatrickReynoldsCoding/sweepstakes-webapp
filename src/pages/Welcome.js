ff205062c20c696ca3bab3e827bf06271ea05ae4
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2022-10-22 15:30:57 +0700

    icon added

--- src/pages/Welcome.js
+++ src/pages/Welcome.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { ReactComponent as YourSvg } from './world-cup.svg';
 
 import "./welcome.css"
 export default function Welcome() {
@@ -8,7 +9,9 @@ export default function Welcome() {
       <div className="welcome-wrapper">
         <div className="welcome-container">
           <div className="welcome-container-row1">
-            <div className="welcome-icon">ICON</div>
+            <div className="welcome-icon">         
+              <YourSvg className="welcome-icon"/>
+            </div>
             <div className="welcome-text">Welcome!</div>
           </div>
           <div className="welcome-container-row2">
@@ -21,8 +24,7 @@ export default function Welcome() {
             to="/registration"
             style={{ textDecoration: "none", color: "none" }}
           >
-            <div className="button" action={"/registration"}>
-              Register
+            <div className="button" action={"/registration"}>Register
             </div>
           </Link>
           <Link
@@ -33,7 +35,7 @@ export default function Welcome() {
               login
             </div>
           </Link>
-      </div>
+      </div>  
     </div>
     </>
   );
